94198b91b815ff49f343240efafe316b001e6928
Greenton <129997930+Greenton@users.noreply.github.com>
2023-05-30 13:11:40 -0700

    Made Levels (0,1,2,3,4,5,6,7,8,9)

---
+++ Death Evasion/Assets/Scripts/GameSession.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType<GameSession>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Death Evasion/Assets/Scripts/Level.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2f;
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(9);
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(4);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene(5);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene(6);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel7()
+    {
+        SceneManager.LoadScene(7);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadLevel8()
+    {
+        SceneManager.LoadScene(8);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
