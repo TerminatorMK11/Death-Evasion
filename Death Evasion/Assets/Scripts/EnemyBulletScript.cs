080caab05fdbc1a4e24e949cc3adca4457def6b0
TerminatorMK11 <2495540@edu.burnabyschools.ca>
2023-06-05 13:05:30 -0700

    Enemy Dmg

--- Death Evasion/Assets/Scripts/EnemyBulletScript.cs
+++ Death Evasion/Assets/Scripts/EnemyBulletScript.cs
@@ -26,4 +26,12 @@ public class EnemyBulletScript : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FindObjectOfType<Player>().TakeDamage(5);
+        }
+
+    }
 }
