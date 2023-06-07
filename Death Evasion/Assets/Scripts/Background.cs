03706b7de51b369a362bbc34be8e794ca7db4286
Chevynova013 <1085685@edu.burnabyschools.ca>
2023-06-07 15:04:58 -0700

    Made Level 5 background

--- Death Evasion/Assets/Scripts/Background.cs
+++ Death Evasion/Assets/Scripts/Background.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameCamera : MonoBehaviour
+public class Background : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public Transform player;
@@ -15,6 +15,6 @@ public class GameCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x + 6, player.position.y, player.position.z - 10);
+        transform.position = new Vector3(player.position.x +6, player.position.y, player.position.z -9);
     }
 }
